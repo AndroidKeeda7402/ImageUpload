5aa533cb1dc3e977949eb3fcffd8d9a1b4e5b5dc
43065 <khan.moin7402@gmail.com>
2017-09-18 11:54:44 +0530

    Initial commit

---
+++ app/src/main/java/code/imageupload/androidkeeda/com/imageupload/AppConfig.java
@@ -0,0 +1,12 @@
+package code.imageupload.androidkeeda.com.imageupload;
+
+/**
+ * Created by Moin Khan on 2/21/2017.
+ */
+public class AppConfig {
+
+    // local
+    public static String BASE_URL = "http://192.168.20.137/webservicev2/";
+
+    public static String uploadImage = BASE_URL + "uploadImage.php";
+}
